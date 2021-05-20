64854aaf73d7e5caf50f9b323d32d17417878657
Marina Viriyalova <m.viriyalova@gmail.com>
2021-05-20 07:28:42 +0300

    fixed popup, footer,buttons, profile title ans js

--- script/script.js
+++ script/script.js
@@ -3,26 +3,27 @@ let openBlockPopup = document.querySelector('.profile__edit-button');
 let closeBlockPopup = blockPopup.querySelector('.popup__close');
 let nameFromPage = document.querySelector('.profile__title-text');
 let jobFromPage = document.querySelector('.profile__subtitle-text');
-let nameFromForm = document.querySelector('.popup__input_name');
-let jobFromForm = document.querySelector('.popup__input_work');
+let nameFromForm = document.querySelector('.popup__input_text_name');
+let jobFromForm = document.querySelector('.popup__input_text_work');
 
-function togglePopup() {
-    blockPopup.classList.toggle('popup_opened');
+// открытие popup
+function openPopup() {
+    blockPopup.classList.add('popup_opened');
     nameFromForm.value = nameFromPage.textContent;
     jobFromForm.value = jobFromPage.textContent;
 }
-
+// закрытие popup
 function closePopup() {
     blockPopup.classList.remove('popup_opened');
 }
-
-openBlockPopup.addEventListener('click', togglePopup);
-closeBlockPopup.addEventListener('click', togglePopup);
-
+// сохранение изменений
 function savePopup(event) {
     event.preventDefault();
     nameFromPage.textContent = nameFromForm.value;
     jobFromPage.textContent = jobFromForm.value;
     closePopup();
 }
+// слушатели 
+openBlockPopup.addEventListener('click', openPopup);
+closeBlockPopup.addEventListener('click', closePopup);
 blockPopup.addEventListener('submit', savePopup);
